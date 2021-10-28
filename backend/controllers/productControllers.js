113ebe54e95231e1a73974f6550d84e0d8ba79dd
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-10-28 17:22:26 +0300

    Changed add product route

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -65,7 +65,7 @@ const addProduct = asyncHandler(async (req, res) => {
       await newProduct.save()
     }
     if (countInStock) {
-      newProduct.image = countInStock
+      newProduct.countInStock = countInStock
       await newProduct.save()
     }
 
