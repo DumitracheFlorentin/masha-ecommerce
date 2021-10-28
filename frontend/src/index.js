e6959687d7ae1f80f6d9e9c78072d1b3db2fb776
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-10-28 17:54:07 +0300

    Added state management - redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,21 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import { BrowserRouter as Router } from "react-router-dom"
+
+// Import Redux
+import { Provider } from "react-redux"
+import store from "./store"
+
+// Import Bootstrap Theme
+import "./bootstrap.min.css"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 )
 
