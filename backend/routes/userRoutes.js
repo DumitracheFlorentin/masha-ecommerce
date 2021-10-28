f0a97325123b780e77328a153c37b204a851a770
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-10-28 13:48:55 +0300

    Deleted a wrong comment

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,12 +10,9 @@ import {
 
 const router = express.Router()
 
-// User Routes
 router.route("/").get(getAllUsers)
 router.route("/:id").get(getUser).delete(deleteUser).patch(updateUser)
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
-
-// Product Routes
 
 export default router
