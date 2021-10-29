25aec615350b0acf010627bca418b327d5aee86b
Florentin <dumitracheflorentincristian@gmail.com>
2021-10-29 23:46:17 +0300

    Added redirect to login from cart in case the client is not logged in

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,15 +1,22 @@
-import { useLocation } from "react-router-dom"
+import { useEffect } from "react"
+import { useLocation, useHistory } from "react-router-dom"
 
 // Import Components
 import Navigation from "../components/Navigation"
 
 export default function Cart() {
   const location = useLocation()
+  const history = useHistory()
 
   const id = location.pathname.split("/")[2]
   const qty = location.search.split("=")[1]
 
-  console.log(id, qty)
+  useEffect(() => {
+    if (!localStorage.getItem("masha-user-token")) {
+      history.push("/login")
+    }
+  }, [])
+
   return (
     <>
       <Navigation />
