1a420786744aa8c8d3a93575094b98b463269e11
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-11-01 23:06:23 +0200

    Added all routes for orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,17 @@
 import express from "express"
-import { createOrder } from "../controllers/orderControllers.js"
+import {
+  createOrder,
+  deleteOrder,
+  allOrders,
+  specificOrders,
+} from "../controllers/orderControllers.js"
 import { protectedRoute } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
+router.route("/").get(allOrders)
 router.route("/create").post(protectedRoute, createOrder)
+router.route("/delete").delete(deleteOrder)
+router.route("/specific").get(protectedRoute, specificOrders)
 
 export default router
